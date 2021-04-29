10cdb61b6f84e942831b817dfdbd2492101c16f4
DanielBlack <bkw1402@naver.com>
2021-04-30 01:21:39 +0900

    Update gui.py
    출력시 가운데 정렬

--- GUI/gui.py
+++ GUI/gui.py
@@ -8,14 +8,14 @@ class DB(QWidget):
         super().__init__()
         self.table = QTableWidget()
         self.initUI()
-       
+
     def initUI(self):
         appname = QLabel('choose app')
         kakaoButton = QPushButton("KakaoTalk")
         wechatButton = QPushButton("WeChat")
         lysnButton = QPushButton("Lysn")
         tongButton = QPushButton("TongTong")
-    
+
         lysnButton.clicked.connect(self.lysnButtonClicked)
 
         hButtonbox1 = QHBoxLayout()
@@ -32,10 +32,17 @@ class DB(QWidget):
         layout.addWidget(self.table)
 
         self.setLayout(layout)
-
         self.setGeometry(1000,1000,1000,1000)
         self.setWindowTitle('messenger app artifact')
-        
+        self.center()
+        self.show()
+
+    def center(self):
+        frame_info = self.frameGeometry()
+        display_center = QDesktopWidget().availableGeometry().center()
+        frame_info.moveCenter(display_center)
+        self.move(frame_info.topLeft())
+
     def lysnButtonClicked(self):
         global filename
         filename = QFileDialog.getOpenFileName(self, 'Open File')
@@ -44,7 +51,7 @@ class DB(QWidget):
             colname, rowlist = lysn_userDB(filename[0])
         elif filename[0][-7:] == 'talk.db':
             colname, rowlist = lysn_talkDB(filename[0])
-        
+
         self.table.setSelectionMode(QAbstractItemView.SingleSelection)
         self.table.setEditTriggers(QAbstractItemView.NoEditTriggers)
         self.table.setColumnCount(len(colname))
@@ -55,8 +62,8 @@ class DB(QWidget):
         for i in range(len(rowlist)):
             for j in range(len(rowlist[i])):
                 self.table.setItem(i, j, QTableWidgetItem(str(rowlist[i][j])))
-    
-        
+
+
 app = QApplication(sys.argv)
 db = DB()
 db.show()
