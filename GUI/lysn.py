d40615e51611182125b5a597d35dc2109a9c7272
K-subin <kimsubin1359@gmail.com>
2021-05-01 19:06:20 +0900

    convert idx to name

--- GUI/lysn.py
+++ GUI/lysn.py
@@ -21,7 +21,7 @@ def lysn_talkDB(dbfile, android_id):
     udbfile=dbfile.replace('talk.db', 'user.db')
     ulist = lysn_userDB(udbfile, android_id)[1]
     useridx = ulist[0][1]
-
+    
     # db 열기
     db = sqlcipher.connect(dbfile)
     talkkey = talkDB_key(useridx)
@@ -31,6 +31,13 @@ def lysn_talkDB(dbfile, android_id):
     # 추출할 내용 지정
     colname = {'time':'시간', 'sender':'보낸사람', 'type':'타입', 'roomidx':'채팅방 번호', 'text':'메시지'}
     rowlist = export(cur, 'chats', colname) # talk.db에 chats 테이블 내용 추출
+    
+    # 이름과 고유번호 가져오기
+    for i in range(len(rowlist)):
+        for urow in ulist:
+            if rowlist[i][1] == urow[1]:
+                rowlist[i][1] = urow[0]
+    
     return colname.values(), rowlist
 
 # 내용 추출하기
@@ -63,5 +70,3 @@ if __name__ == '__main__':
     print(colname)
     for row in rowlist:
         print(row)
-    
-      
