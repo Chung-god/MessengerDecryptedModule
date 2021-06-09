4a4aa0352da31adb78f4e502d87de678bcf7789d
parkjuwon-1 <82521308+parkjuwon-1@users.noreply.github.com>
2021-06-09 09:29:08 +0900

    Add files via upload

---
+++ wechat_key.py
@@ -0,0 +1,47 @@
+from xml.etree.ElementTree import parse
+from xml.etree.ElementTree import Element, dump, ElementTree
+import subprocess
+import hashlib
+
+
+def wechat_imei():
+    imei = subprocess.check_output('adb shell "service call iphonesubinfo 4 | cut -c 52-66 | tr -d \'.[:space:]\'"', shell=True, encoding='utf-8')
+    return imei
+
+def wechat_ua():
+
+    tree = parse('com.tencent.mm_preferences.xml')
+    root = tree.getroot()
+
+    map= (root.findall("string"))
+    uin = map[4].text
+    return uin
+
+def wechat_en(UIN,IMEI):
+    
+    UIN2 = UIN.encode()
+    IMEI2 = IMEI.encode()
+
+
+    EN_string = IMEI2 + UIN2
+    EN_key = hashlib.md5(EN_string).hexdigest()
+    key = EN_key[:7]
+    
+    return key
+
+def wechat_path():
+    wpath = subprocess.check_output('find ./ -name "EnMicroMsg.db"', shell=True, encoding='utf-8')
+    root = wpath[18:50]
+    return root
+
+if __name__=='__main__':
+    Imei={}
+    Uin={}
+    Acc={}
+    Key={}
+    Root={}
+    
+    Imei=wechat_imei(Imei)
+    Uin=wechat_ua(Uin)
+    Key=wechat_en(Uin,Imei)
+    Root=wechat_path(Root)
